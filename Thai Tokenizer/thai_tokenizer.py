b57ca2c1bd05eee3c935c332873d7fd8cb38c7f4
Sophia Marx <sophiamarx@bennington.edu>
2019-03-13 16:18:05 -0400

    partway through thai

--- Thai Tokenizer/thai_tokenizer.py
+++ Thai Tokenizer/thai_tokenizer.py
@@ -2,8 +2,8 @@
 
 import sys
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+#input_file = sys.argv[1]
+#output_file = sys.argv[2]
 
 v1 = ['เ','แ','โ','ใ','ไ']
 c1 = ['ก', 'ข', 'ฃ', 'ค', 'ฅ', 'ฆ', 'ง', 'จ', 'ฉ', 'ช', 'ซ', 'ฌ', 'ญ', 'ฎ', 'ฏ', 'ฐ', 'ฑ', 'ฒ', 'ณ', 'ด', 'ต', 'ถ', 'ท', 'ธ', 'น', 'บ', 'ป', 'ผ', 'ฝ', 'พ', 'ฟ', 'ภ', 'ม', 'ย', 'ร', 'ฤ', 'ล', 'ฦ', 'ว', 'ศ', 'ษ', 'ส', 'ห', 'ฬ', 'อ', 'ฮ']
@@ -13,11 +13,22 @@ t = ['\u0E48', '\u0E49', '\u0E4A', '\u0E4B']
 v3 = ['า', 'อ', 'ย', 'ว']
 c3 = ['ง', 'น', 'ม', 'ด', 'บ', 'ก', 'ย', 'ว']
 
-def tokenize(line):
+def tokenize(): #line is the argument
 	#define a function that will take in a line of Thai text (without spaces), and return the line with spaces between the words
+	curr_state = None #tracks the current state, int
+	start_state = 0 #keeps the int start state, shouldn't change
+	end_states = [] #keeps a list of valid end states
+	state_transitions = [] #2-d array representing the state transition matrix
 
-with open(in_file, 'r') as open_in:
+	state_transitions = [[1,2,None,None,None,None,None,None],[None,2,None,None,None,None,None,None,],[7,8,3,4,5,6,9,None],[None,None,None,4,5,6,9,None],[7,8,None,None,5,6,9,None],[7,8,None,None,None,None,9,None],[7,8,None,None,None,None,9,None],[None,None,None,None,None,None,None,1],[None,None,None,None,None,None,None,2],[None,None,None,None,None,None,None,0]]
+	for i in range(0, len(state_transitions)):
+		print(state_transitions[i])
+
+tokenize()
+
+
+"""with open(in_file, 'r') as open_in:
 	with open(out_file, 'w') as open_out:
 		for line in open_in.readlines():
 			spaced_line = tokenize(line)
-			open_out.write(spaced_line)
+			open_out.write(spaced_line) """
